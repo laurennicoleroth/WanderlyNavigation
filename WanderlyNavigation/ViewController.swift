235ae32c288104a8e4c317fb87544c0fb2d1b515
Lauren N. Roth <laurennicoleroth@gmail.com>
2019-03-05 11:59:22 -0500

    Initial project files

---
+++ WanderlyNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WanderlyNavigation
+//
+//  Created by Lauren Nicole Roth on 3/5/19.
+//  Copyright © 2019 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
